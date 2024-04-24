cdb68f65c7c99da762ccb227675e26fab15ddc4e
negert <niklas.egert@worldiety.de>
2024-04-24 15:06:51 +0200

    SVG korrigiert und gestyled

--- web/vuejs/src/components/UiErrorMessage.vue
+++ web/vuejs/src/components/UiErrorMessage.vue
@@ -32,7 +32,7 @@ function reload(): void {
 					<div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
 						<div class="sm:flex sm:items-start">
 							<div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-								<Alert/>
+								<Alert class="w-7 pb-1.5 text-red-500"/>
 							</div>
 							<div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
 								<div class="mt-2">
