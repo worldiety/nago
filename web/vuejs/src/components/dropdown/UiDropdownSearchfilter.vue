a0da6ad2863853de028672e07854be238f302181
negert <niklas.egert@worldiety.de>
2024-05-03 13:35:50 +0200

    Suchfeld und svg für Darkmode angepasst

--- web/vuejs/src/components/dropdown/UiDropdownSearchfilter.vue
+++ web/vuejs/src/components/dropdown/UiDropdownSearchfilter.vue
@@ -14,8 +14,8 @@ const searchQuery = ref<string>('')
 
 <template>
 	<div class="border-b-2 border-ora-orange relative p-2.5 mx-2  ">
-		<input v-model="searchQuery" @input="$emit('searchQueryChanged', searchQuery)" class="outline-none w-full cursor-text pl-6 dark:bg-ora-dropdown-background"  type="text" placeholder="Suchen nach...">
-		<Magnifier class="absolute w-5 h-5 top-0 left-0 mt-3 ml-2"/>
+		<input v-model="searchQuery" @input="$emit('searchQueryChanged', searchQuery)" class="outline-none w-full cursor-text pl-6 dark:bg-ora-dropdown-background dark:text-white" type="text" placeholder="Suchen nach...">
+		<Magnifier class="absolute w-5 h-5 top-0 left-0 mt-3 ml-2 dark:text-white"/>
 	</div>
 </template>
 
