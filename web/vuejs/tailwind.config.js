72cd1a1881cebaf81bf3a32fe8df6e0344f1664e
Lukas Gerdes <lukas.gerdes@worldiety.de>
2024-05-28 11:13:07 +0200

    WDYNAGO-6 Removed unnecessary code from tailwind config

--- web/vuejs/tailwind.config.js
+++ web/vuejs/tailwind.config.js
@@ -34,19 +34,5 @@ export default {
 			},
 		},
 	},
-
-	mode: 'jit',
 	darkMode: 'class',
-	// safelist does not really work for our dynamic stuff
-	// safelist: ["gap-[2fr]", "grid", "grid-cols-2",]
-	safelist: ["grid",
-		"gap-1","gap-2","gap-3","gap-4","text-sm","text-base","text-lg","text-xl","text-2xl",
-		{ pattern: /grid-cols-(1|2|3|4|5|6|7|8|9|10|11|12)/ , variants: ['sm', 'md', 'lg', 'xl', '2xl'],},
-		{ pattern: /col-span-(1|2|3|4|5|6|7|8|9|10|11|12)/ , variants: ['sm', 'md', 'lg', 'xl', '2xl'],},
-		{ pattern: /grid-rows-(1|2|3|4|5|6)/ },
-		{ pattern: /col-start-(1|2|3|4|5|6|7|8|9|10|11|12)/ },
-		{ pattern: /col-end-(1|2|3|4|5|6|7|8|9|10|11|12)/ },
-		{ pattern: /row-start-(1|2|3|4|5|6|7|8|9|10|11|12)/ },
-		{ pattern: /row-end-(1|2|3|4|5|6|7|8|9|10|11|12)/ },
-	]
 }
