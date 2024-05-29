d9fafca7869f4ae20241e2165987258462dc014d
Torben Schinke <torben.schinke@worldiety.de>
2024-05-29 17:29:39 +0200

    removed overflow-y-auto because it makes dropdown unusable in dialogs

--- web/vuejs/src/components/UiDialog.vue
+++ web/vuejs/src/components/UiDialog.vue
@@ -14,7 +14,7 @@
 
 			<!-- Dialog body -->
 			<!-- TODO @Lukas: overflow-y-auto klappt in der Praxis für normale Dialoge mit Dropdown nicht, da es im overflow scrollt. Bei fullscreen height wäre das aber korrekt?-->
-			<div class="bg-white overflow-y-auto pt-3.5 px-6 pb-6" :class="{'rounded-b-xl': !ui.footer.v}">
+			<div class="bg-white pt-3.5 px-6 pb-6" :class="{'rounded-b-xl': !ui.footer.v}">
 				<UiGeneric :ui="ui.body.v" />
 			</div>
 
