70616a6427d82e21c8b7b3dfb9f5c9f81e41690e
Torben Schinke <torben.schinke@worldiety.de>
2024-06-03 18:01:57 +0200

    removed more mutex locks from scopes lifecycle

--- presentation/core/scopes.go
+++ presentation/core/scopes.go
@@ -1,22 +1,22 @@
 package core
 
 import (
+	"go.wdy.de/nago/pkg/std/concurrent"
 	"go.wdy.de/nago/presentation/ora"
 	"log/slog"
-	"sync"
 	"time"
 )
 
 // Scopes manages all available scopes and their lifetimes.
 type Scopes struct {
-	mutex  sync.Mutex
-	ticker *time.Ticker
-	done   chan bool
-	scopes map[ora.ScopeID]*Scope
+	ticker    *time.Ticker
+	done      chan bool
+	scopes    concurrent.CoWMap[ora.ScopeID, *Scope]
+	destroyed concurrent.Value[bool]
 }
 
 func NewScopes() *Scopes {
-	s := &Scopes{ticker: time.NewTicker(time.Minute), done: make(chan bool), scopes: make(map[ora.ScopeID]*Scope)}
+	s := &Scopes{ticker: time.NewTicker(time.Minute), done: make(chan bool)}
 	go func() {
 		for {
 			select {
@@ -32,44 +32,40 @@ func NewScopes() *Scopes {
 }
 
 func (s *Scopes) Get(id ora.ScopeID) (*Scope, bool) {
-	s.mutex.Lock()
-	defer s.mutex.Unlock()
-
-	scope, ok := s.scopes[id]
+	scope, ok := s.scopes.Get(id)
 	return scope, ok
 }
 
 func (s *Scopes) Put(scope *Scope) {
-	s.mutex.Lock()
-	defer s.mutex.Unlock()
-
-	s.scopes[scope.id] = scope
+	s.scopes.Put(scope.id, scope)
 }
 
 // tick checks all scopes and destroys all scopes which reached EOL.
 func (s *Scopes) tick(now time.Time) {
-	s.mutex.Lock()
-	defer s.mutex.Unlock()
-
-	for _, scope := range s.scopes {
+	s.scopes.Each(func(key ora.ScopeID, scope *Scope) bool {
 		if now.After(scope.EOL()) {
 			slog.Info("scope is end of life and now destroyed", slog.String("id", string(scope.id)))
-			delete(s.scopes, scope.id)
+			s.scopes.Delete(scope.id)
 			scope.Destroy()
 		}
-	}
+
+		return true
+	})
+
 }
 
 // Destroy stops the internal timer and frees all contained scopes.
 func (s *Scopes) Destroy() {
-	s.mutex.Lock()
-	defer s.mutex.Unlock()
+	if !concurrent.CompareAndSwap(&s.destroyed, false, true) {
+		return
+	}
 
 	s.ticker.Stop()
 
-	for _, scope := range s.scopes {
+	s.scopes.Each(func(key ora.ScopeID, scope *Scope) bool {
 		scope.Destroy()
-	}
+		return true
+	})
 
-	clear(s.scopes)
+	s.scopes.Clear()
 }
