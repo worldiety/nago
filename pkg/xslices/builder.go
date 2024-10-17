063124c2ce93df8b08d40132a0fc277c39b1daae
Torben Schinke <torben.schinke@worldiety.de>
2024-10-17 18:27:27 +0200

    added xslices builder

---
+++ pkg/xslices/builder.go
@@ -0,0 +1,30 @@
+package xslices
+
+import (
+	"iter"
+	"slices"
+)
+
+type Builder[T any] struct {
+	tmp []T
+}
+
+func (b *Builder[T]) Append(v ...T) *Builder[T] {
+	b.tmp = append(b.tmp, v...)
+	return b
+}
+
+func (b *Builder[T]) Len() int {
+	return len(b.tmp)
+}
+
+// Collect returns a mutable copied slice of the current state. The returned slice can be modified freely and
+// future appends will not corrupt any prior returned slices.
+func (b *Builder[T]) Collect() []T {
+	return slices.Clone(b.tmp)
+}
+
+// All returns a read only view to iterate on of the current state.
+func (b *Builder[T]) All() iter.Seq[T] {
+	return slices.Values(b.tmp)
+}
