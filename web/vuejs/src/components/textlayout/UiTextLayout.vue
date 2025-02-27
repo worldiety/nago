44869565984366de9c38145299abc00eb6b17fd0
Torben Schinke <torben.schinke@worldiety.de>
2025-02-27 15:56:51 +0100

    fixed text layout and added eventbus example

--- web/vuejs/src/components/textlayout/UiTextLayout.vue
+++ web/vuejs/src/components/textlayout/UiTextLayout.vue
@@ -6,8 +6,7 @@ import {fontCSS} from '@/components/shared/font';
 import {frameCSS} from '@/components/shared/frame';
 import {paddingCSS} from '@/components/shared/padding';
 import {useServiceAdapter} from '@/composables/serviceAdapter';
-import {TextLayout} from '@/shared/protocol/ora/textLayout';
-import {FunctionCallRequested} from "@/shared/proto/nprotoc_gen";
+import {FunctionCallRequested, TextLayout} from "@/shared/proto/nprotoc_gen";
 import {nextRID} from "@/eventhandling";
 
 const props = defineProps<{
@@ -17,9 +16,9 @@ const props = defineProps<{
 const serviceAdapter = useServiceAdapter();
 
 function onClick() {
-	if (props.ui.t) {
+	if (props.ui.action) {
 		serviceAdapter.sendEvent(new FunctionCallRequested(
-			props.ui.t,
+			props.ui.action,
 			nextRID(),
 		));
 	}
@@ -27,13 +26,13 @@ function onClick() {
 
 // copy-paste me into UiText, UiVStack and UiHStack (or refactor me into some kind of generics-getter-setter-nightmare).
 function commonStyles(): string[] {
-	let styles = frameCSS(props.ui.f);
+	let styles = frameCSS(props.ui.frame);
 
-	styles.push(...borderCSS(props.ui.b));
+	styles.push(...borderCSS(props.ui.border));
 
 	// other stuff
-	styles.push(...paddingCSS(props.ui.p));
-	styles.push(...fontCSS(props.ui.fn));
+	styles.push(...paddingCSS(props.ui.padding));
+	styles.push(...fontCSS(props.ui.font));
 
 	return styles;
 }
@@ -47,7 +46,7 @@ const frameStyles = computed<string>(() => {
 const clazz = computed<string>(() => {
 	let classes = [];
 
-	if (props.ui.t) {
+	if (props.ui.action) {
 		classes.push('cursor-pointer');
 	}
 
@@ -57,7 +56,7 @@ const clazz = computed<string>(() => {
 
 <template>
 	<!-- textlayout -->
-	<div v-if="!props.ui.iv" :class="clazz" :style="frameStyles" @click="onClick">
-		<ui-generic v-for="ui in props.ui.c" :ui="ui"/>
+	<div v-if="!props.ui.invisible" :class="clazz" :style="frameStyles" @click="onClick">
+		<ui-generic v-for="ui in props.ui.children.value" :ui="ui"/>
 	</div>
 </template>
