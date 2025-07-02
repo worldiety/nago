2f652dccd54fa0e85500ce7169d6c29fb04114ed
Lukas Freesemann <lukas.freesemann@worldiety.de>
2025-07-02 09:35:06 +0200

    feat: add apex charts, add bar chart

---
+++ web/vuejs/src/components/charts/UiApexChartsGeneric.vue
@@ -0,0 +1,21 @@
+<!--
+ Copyright (c) 2025 worldiety GmbH
+
+ This file is part of the NAGO Low-Code Platform.
+ Licensed under the terms specified in the LICENSE file.
+
+ SPDX-License-Identifier: Custom-License
+-->
+
+<script lang="ts" setup>
+import type { Component as OraComponent } from '@/shared/proto/nprotoc_gen';
+import { vueComponentFor } from '@/shared/uiComponents';
+
+const props = defineProps<{
+	ui: OraComponent; // if we don't rename the import, Vue confuses everything up
+}>();
+</script>
+
+<template>
+	<component :is="vueComponentFor(props.ui)" :ui="props.ui" />
+</template>
