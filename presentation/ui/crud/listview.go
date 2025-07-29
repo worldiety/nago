b54862472c04f5f46a6ecb2765a430a2ac15abc7
benediktdittel <benedikt.dittel@worldiety.de>
2025-07-29 10:45:40 +0200

    chore: new type for crud.List

--- presentation/ui/crud/listview.go
+++ presentation/ui/crud/listview.go
@@ -16,9 +16,27 @@ import (
 	"slices"
 )
 
-func List[Entity data.Aggregate[ID], ID data.IDType](opts TOptions[Entity, ID]) ui.DecoredView {
-	ds := opts.datasource()
-	bnd := opts.bnd
+// TList is a crud component(CRUD List).
+type TList[Entity data.Aggregate[ID], ID data.IDType] struct {
+	opts TOptions[Entity, ID]
+
+	padding            ui.Padding
+	frame              ui.Frame
+	border             ui.Border
+	accessibilityLabel string
+	invisible          bool
+}
+
+func List[Entity data.Aggregate[ID], ID data.IDType](opts TOptions[Entity, ID]) TList[Entity, ID] {
+	return TList[Entity, ID]{
+		opts:  opts,
+		frame: ui.Frame{}.FullWidth(),
+	}
+}
+
+func (t TList[Entity, ID]) Render(ctx core.RenderContext) core.RenderNode {
+	ds := t.opts.datasource()
+	bnd := t.opts.bnd
 
 	var count int
 	return ui.VStack(
@@ -32,6 +50,40 @@ func List[Entity data.Aggregate[ID], ID data.IDType](opts TOptions[Entity, ID]) 
 		})...).Caption(ui.Text("Alle Einträge")).
 			Footer(ui.Text(fmt.Sprintf("%d von %d Einträgen", count, ds.totalCount))).
 			Frame(ui.Frame{}.FullWidth()),
-	).FullWidth()
+	).Visible(!t.invisible).
+		Frame(t.frame).
+		Border(t.border).
+		Padding(t.padding).
+		AccessibilityLabel(t.accessibilityLabel).
+		Render(ctx)
+}
 
+func (t TList[Entity, ID]) Padding(padding ui.Padding) ui.DecoredView {
+	t.padding = padding
+	return t
+}
+
+func (t TList[Entity, ID]) WithFrame(fn func(ui.Frame) ui.Frame) ui.DecoredView {
+	t.frame = fn(t.frame)
+	return t
+}
+
+func (t TList[Entity, ID]) Frame(frame ui.Frame) ui.DecoredView {
+	t.frame = frame
+	return t
+}
+
+func (t TList[Entity, ID]) Border(border ui.Border) ui.DecoredView {
+	t.border = border
+	return t
+}
+
+func (t TList[Entity, ID]) Visible(visible bool) ui.DecoredView {
+	t.invisible = !visible
+	return t
+}
+
+func (t TList[Entity, ID]) AccessibilityLabel(label string) ui.DecoredView {
+	t.accessibilityLabel = label
+	return t
 }
