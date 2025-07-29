0e081c5d68980390e301d4385cec146da7838b5f
benediktdittel <benedikt.dittel@worldiety.de>
2025-07-29 10:44:36 +0200

    chore: new type for crud.Cards

--- presentation/ui/crud/cards.go
+++ presentation/ui/crud/cards.go
@@ -15,16 +15,72 @@ import (
 	"slices"
 )
 
-func Cards[Entity data.Aggregate[ID], ID data.IDType](opts TOptions[Entity, ID]) ui.DecoredView {
-	ds := opts.datasource()
-	bnd := opts.bnd
+// TCards is a crud component(CRUD Cards).
+type TCards[Entity data.Aggregate[ID], ID data.IDType] struct {
+	opts TOptions[Entity, ID]
+
+	padding            ui.Padding
+	gap                ui.Length
+	frame              ui.Frame
+	border             ui.Border
+	accessibilityLabel string
+	invisible          bool
+}
+
+func Cards[Entity data.Aggregate[ID], ID data.IDType](opts TOptions[Entity, ID]) TCards[Entity, ID] {
+	return TCards[Entity, ID]{
+		opts:    opts,
+		padding: ui.Padding{}.All(ui.L16),
+		gap:     ui.L16,
+	}
+}
+
+func (t TCards[Entity, ID]) Render(ctx core.RenderContext) core.RenderNode {
+	ds := t.opts.datasource()
+	bnd := t.opts.bnd
 
 	return ui.VStack(
 		ui.Each(slices.Values(ds.List()), func(entity Entity) core.View {
-			entityState := core.StateOf[Entity](opts.wnd, fmt.Sprintf("crud.card.entity.%v", entity.Identity())).Init(func() Entity {
+			entityState := core.StateOf[Entity](t.opts.wnd, fmt.Sprintf("crud.card.entity.%v", entity.Identity())).Init(func() Entity {
 				return entity
 			})
 			return Card[Entity](bnd, entityState).Frame(ui.Frame{}.FullWidth())
 		})...,
-	).Gap(ui.L16).Padding(ui.Padding{}.All(ui.L16))
+	).Gap(t.gap).
+		Visible(!t.invisible).
+		Frame(t.frame).
+		Border(t.border).
+		Padding(t.padding).
+		AccessibilityLabel(t.accessibilityLabel).
+		Render(ctx)
+}
+
+func (t TCards[Entity, ID]) Padding(padding ui.Padding) ui.DecoredView {
+	t.padding = padding
+	return t
+}
+
+func (t TCards[Entity, ID]) WithFrame(fn func(ui.Frame) ui.Frame) ui.DecoredView {
+	t.frame = fn(t.frame)
+	return t
+}
+
+func (t TCards[Entity, ID]) Frame(frame ui.Frame) ui.DecoredView {
+	t.frame = frame
+	return t
+}
+
+func (t TCards[Entity, ID]) Border(border ui.Border) ui.DecoredView {
+	t.border = border
+	return t
+}
+
+func (t TCards[Entity, ID]) Visible(visible bool) ui.DecoredView {
+	t.invisible = !visible
+	return t
+}
+
+func (t TCards[Entity, ID]) AccessibilityLabel(label string) ui.DecoredView {
+	t.accessibilityLabel = label
+	return t
 }
