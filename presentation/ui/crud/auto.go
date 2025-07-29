a97bd22ecbda8171513179386fce173a6964b113
benediktdittel <benedikt.dittel@worldiety.de>
2025-07-29 10:43:04 +0200

    chore: new type for crud.AutoRootView & crud.AutoView

--- presentation/ui/crud/auto.go
+++ presentation/ui/crud/auto.go
@@ -25,13 +25,30 @@ type AutoRootViewOptions struct {
 	CreateDisabled bool
 }
 
+// TAutoRootView is a crud component(CRUD Auto Root View).
+type TAutoRootView[E form.Aggregate[E, ID], ID ~string] struct {
+	opts     AutoRootViewOptions
+	useCases rcrud.UseCases[E, ID]
+
+	frame ui.Frame
+}
+
 func AutoRootView[E form.Aggregate[E, ID], ID ~string](opts AutoRootViewOptions, useCases rcrud.UseCases[E, ID]) func(wnd core.Window) core.View {
 	return func(wnd core.Window) core.View {
-		bnd := AutoBinding[E](AutoBindingOptions{}, wnd, useCases)
-		return ui.VStack(
-			AutoView(AutoViewOptions{Title: opts.Title, CreateDisabled: opts.CreateDisabled}, bnd, useCases),
-		).FullWidth()
+		return TAutoRootView[E, ID]{
+			opts:     opts,
+			useCases: useCases,
+			frame:    ui.Frame{}.FullWidth(),
+		}
 	}
+}
+
+func (t TAutoRootView[E, ID]) Render(ctx core.RenderContext) core.RenderNode {
+	bnd := AutoBinding[E](AutoBindingOptions{}, ctx.Window(), t.useCases)
+	return ui.VStack(
+		AutoView(AutoViewOptions{Title: t.opts.Title, CreateDisabled: t.opts.CreateDisabled}, bnd, t.useCases),
+	).Frame(t.frame).
+		Render(ctx)
 }
 
 type AutoViewOptions struct {
@@ -56,46 +73,74 @@ func canUpdate[E form.Aggregate[E, ID], ID ~string](bnd *Binding[E], useCases rc
 	return bnd.wnd.Subject().HasPermission(useCases.PermUpdate())
 }
 
+// TAutoView is a crud component(CRUD Auto View).
 // AutoView creates a real simple default CRUD view for rapid prototyping.
+type TAutoView[E form.Aggregate[E, ID], ID ~string] struct {
+	opts     AutoViewOptions
+	bnd      *Binding[E]
+	useCases rcrud.UseCases[E, ID]
+
+	padding   ui.Padding
+	alignment ui.Alignment
+	frame     ui.Frame
+}
+
 func AutoView[E form.Aggregate[E, ID], ID ~string](opts AutoViewOptions, bnd *Binding[E], usecases rcrud.UseCases[E, ID]) core.View {
-	if !canFindAll(bnd, usecases) {
+	return TAutoView[E, ID]{
+		opts:      opts,
+		bnd:       bnd,
+		useCases:  usecases,
+		padding:   ui.Padding{Top: ui.L40},
+		alignment: ui.Leading,
+		frame:     ui.Frame{}.FullWidth(),
+	}
+}
+
+func (t TAutoView[E, ID]) Render(ctx core.RenderContext) core.RenderNode {
+	if !canFindAll(t.bnd, t.useCases) {
 		return ui.VStack(
 
-			ui.H1(opts.Title),
+			ui.H1(t.opts.Title),
 
-			alert.IfPermissionDenied(bnd.wnd, "."),
-		).Alignment(ui.Leading).Padding(ui.Padding{Top: ui.L40}).Frame(ui.Frame{}.FullWidth())
+			alert.IfPermissionDenied(t.bnd.wnd, "."),
+		).Alignment(t.alignment).
+			Padding(t.padding).
+			Frame(t.frame).
+			Render(ctx)
 	}
 
 	var zeroE E
 	var actions []core.View
-	if !opts.CreateDisabled && canCreate(bnd, usecases) {
-		if opts.CreateButtonForwardTo == "" {
-			actions = append(actions, ButtonCreate(bnd, zeroE, func(d E) (errorText string, infrastructureError error) {
-				if !bnd.Validates(d) {
+	if !t.opts.CreateDisabled && canCreate(t.bnd, t.useCases) {
+		if t.opts.CreateButtonForwardTo == "" {
+			actions = append(actions, ButtonCreate(t.bnd, zeroE, func(d E) (errorText string, infrastructureError error) {
+				if !t.bnd.Validates(d) {
 					return "Bitte prüfen Sie Ihre Eingaben", nil
 				}
 
-				_, err := usecases.Create(bnd.wnd.Subject(), d)
+				_, err := t.useCases.Create(t.bnd.wnd.Subject(), d)
 				return "", err
 			}))
 		} else {
-			actions = append(actions, ButtonCreateForwardTo(bnd, opts.CreateButtonForwardTo, nil))
+			actions = append(actions, ButtonCreateForwardTo(t.bnd, t.opts.CreateButtonForwardTo, nil))
 		}
 	}
 
 	return ui.VStack(
 
-		ui.H1(opts.Title),
+		ui.H1(t.opts.Title),
 
 		View[E, ID](
-			Options[E](bnd).
-				FindAll(usecases.FindAll(bnd.wnd.Subject())).
+			Options[E](t.bnd).
+				FindAll(t.useCases.FindAll(t.bnd.wnd.Subject())).
 				Actions(
 					actions...,
 				),
 		).Frame(ui.Frame{}.FullWidth()),
-	).Alignment(ui.Leading).Padding(ui.Padding{Top: ui.L40}).Frame(ui.Frame{}.FullWidth())
+	).Alignment(t.alignment).
+		Padding(t.padding).
+		Frame(t.frame).
+		Render(ctx)
 }
 
 func NewUseCases[E form.Aggregate[E, ID], ID ~string](permissionPrefix permission.ID, repo data.Repository[E, ID]) rcrud.UseCases[E, ID] {
