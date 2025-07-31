15944f5b3a57215a17b54660ad29b95d8c1d9f1b
benediktdittel <benedikt.dittel@worldiety.de>
2025-07-31 09:39:06 +0200

    chore: new type for form.DialogCreate

--- presentation/ui/form/dialog_create.go
+++ presentation/ui/form/dialog_create.go
@@ -13,46 +13,82 @@ import (
 	"go.wdy.de/nago/presentation/ui/alert"
 )
 
-func DialogCreate[T any](wnd core.Window, name string, presented *core.State[bool], onCreate func(subject auth.Subject, value T) error) core.View {
-	if !presented.Get() {
-		return nil
-	}
-
-	modelState := core.StateOf[T](wnd, "create-"+name)
-
-	return alert.Dialog(
-		name,
-		Auto(AutoOptions{}, modelState),
-		presented,
-		alert.Cancel(nil),
-		alert.Save(func() (close bool) {
-			if err := onCreate(wnd.Subject(), modelState.Get()); err != nil {
-				alert.ShowBannerError(wnd, err)
-				return false
-			}
-
-			return true
-		}),
-	)
+// TDialogCreate is an overlay component(Dialog Create).
+type TDialogCreate[T any] struct {
+	wnd       core.Window
+	name      string
+	presented *core.State[bool]
+	onCreate  func(subject auth.Subject, value T) error
 }
 
-func DialogEdit[T any](wnd core.Window, name string, presented *core.State[bool], modelState *core.State[T], onSave func(subject auth.Subject) error) core.View {
-	if !presented.Get() {
+func DialogCreate[T any](wnd core.Window, name string, presented *core.State[bool], onCreate func(subject auth.Subject, value T) error) TDialogCreate[T] {
+	return TDialogCreate[T]{
+		wnd:       wnd,
+		name:      name,
+		presented: presented,
+		onCreate:  onCreate,
+	}
+}
+
+// TDialogEdit is an overlay component(Dialog Edit).
+type TDialogEdit[T any] struct {
+	wnd        core.Window
+	name       string
+	presented  *core.State[bool]
+	modelState *core.State[T]
+	onSave     func(subject auth.Subject) error
+}
+
+func DialogEdit[T any](wnd core.Window, name string, presented *core.State[bool], modelState *core.State[T], onSave func(subject auth.Subject) error) TDialogEdit[T] {
+	return TDialogEdit[T]{
+		wnd:        wnd,
+		name:       name,
+		presented:  presented,
+		modelState: modelState,
+		onSave:     onSave,
+	}
+}
+
+func (t TDialogEdit[T]) Render(ctx core.RenderContext) core.RenderNode {
+	if !t.presented.Get() {
 		return nil
 	}
 
 	return alert.Dialog(
-		name,
-		Auto(AutoOptions{}, modelState),
-		presented,
+		t.name,
+		Auto(AutoOptions{}, t.modelState),
+		t.presented,
 		alert.Cancel(nil),
 		alert.Save(func() (close bool) {
-			if err := onSave(wnd.Subject()); err != nil {
-				alert.ShowBannerError(wnd, err)
+			if err := t.onSave(t.wnd.Subject()); err != nil {
+				alert.ShowBannerError(t.wnd, err)
 				return false
 			}
 
 			return true
 		}),
-	)
+	).Render(ctx)
+}
+
+func (t TDialogCreate[T]) Render(ctx core.RenderContext) core.RenderNode {
+	if !t.presented.Get() {
+		return nil
+	}
+
+	modelState := core.StateOf[T](t.wnd, "create-"+t.name)
+
+	return alert.Dialog(
+		t.name,
+		Auto(AutoOptions{}, modelState),
+		t.presented,
+		alert.Cancel(nil),
+		alert.Save(func() (close bool) {
+			if err := t.onCreate(t.wnd.Subject(), modelState.Get()); err != nil {
+				alert.ShowBannerError(t.wnd, err)
+				return false
+			}
+
+			return true
+		}),
+	).Render(ctx)
 }
