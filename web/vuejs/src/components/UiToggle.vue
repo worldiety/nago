1470ae95489c5f43245d59e504fcab7c57e227fb
Thomas Riedel <thomas.riedel@worldiety.de>
2026-03-11 11:32:07 +0100

    feat: reimplemented toggle as checkbox

--- web/vuejs/src/components/UiToggle.vue
+++ web/vuejs/src/components/UiToggle.vue
@@ -8,114 +8,69 @@
 -->
 
 <script lang="ts" setup>
-import { ref, watch } from 'vue';
 import { bool2Str } from '@/components/shared/util';
 import { useServiceAdapter } from '@/composables/serviceAdapter';
 import { nextRID } from '@/eventhandling';
-import { Toggle, UpdateStateValueRequested } from '@/shared/proto/nprotoc_gen';
+import type { Toggle } from '@/shared/proto/nprotoc_gen';
+import { UpdateStateValueRequested } from '@/shared/proto/nprotoc_gen';
 
 const props = defineProps<{
 	ui: Toggle;
 }>();
 
 const serviceAdapter = useServiceAdapter();
-const checked = ref<boolean>(props.ui.value ? props.ui.value : false);
 
-watch(
-	() => props.ui.value,
-	(newValue) => {
-		if (newValue) {
-			checked.value = newValue;
-		} else {
-			checked.value = false;
-		}
-	}
-);
-
-function onClick() {
+function toggle() {
 	if (props.ui.disabled) {
 		return;
 	}
 
 	serviceAdapter.sendEvent(
-		new UpdateStateValueRequested(props.ui.inputValue, 0, nextRID(), bool2Str(!checked.value))
+		new UpdateStateValueRequested(props.ui.inputValue, 0, nextRID(), bool2Str(!props.ui.value))
 	);
 }
 </script>
 
 <template>
-	<div
-		style="padding: 1px"
-		v-if="!ui.invisible"
-		class="toggle-switch-container"
-		:class="{ 'toggle-switch-container-disabled': props.ui.disabled }"
-		:tabindex="props.ui.disabled ? '-1' : '0'"
-		@click="onClick"
-		@keydown.enter="onClick"
-		@keydown.space="onClick"
-	>
-		<div class="toggle-switch" :class="{ 'toggle-switch-checked': checked }"></div>
-	</div>
+	<input
+		class="toggle-switch"
+		type="checkbox"
+		:checked="!!props.ui.value"
+		:disabled="props.ui.disabled"
+		@change="toggle"
+	/>
 </template>
 
 <style scoped>
 .toggle-switch {
-	@apply relative h-6 w-11 rounded-full outline outline-1;
-}
+	@apply relative inline-block appearance-none rounded-full outline outline-1 outline-offset-0 w-12 h-6 duration-100 cursor-pointer;
+	@apply focus:outline-I0 focus-visible:outline-I0;
 
-.toggle-switch::after {
-	@apply absolute start-[6px] top-1 h-4 w-4 rounded-full bg-transparent border duration-100 transition-transform content-[''];
-}
-
-.toggle-switch.toggle-switch-checked {
-	@apply after:translate-x-[105%] after:border-M8 bg-I1;
-}
-
-.toggle-switch-container {
-	@apply inline-block rounded-full;
-}
-
-.toggle-switch-container:hover {
-	@apply bg-I0 bg-opacity-25;
-}
-
-.toggle-switch-container:active {
-	@apply bg-opacity-35;
-}
-
-.toggle-switch-container:hover .toggle-switch {
-	@apply outline-I0;
-}
-
-.toggle-switch-container:focus-visible {
-	@apply outline-none;
-
-	.toggle-switch {
-		@apply outline-I0;
+	&:before {
+		content: '';
+		@apply block size-5 absolute top-0.5 left-0.5 rounded-full border border-current duration-100 origin-center;
 	}
-}
 
-.toggle-switch-container.toggle-switch-container-disabled:hover {
-	@apply bg-transparent;
-}
+	&:checked {
+		@apply bg-I0/50;
 
-.toggle-switch-container.toggle-switch-container-disabled:focus-visible {
-	@apply outline-none ring-0;
-}
+		&:before {
+			@apply left-full -translate-x-full -ml-0.5 bg-current;
+		}
+	}
 
-.toggle-switch-container.toggle-switch-container-disabled .toggle-switch {
-	@apply outline-ST0;
-}
+	&:hover {
+		@apply bg-I0/25;
+	}
 
-.toggle-switch-container.toggle-switch-container-disabled .toggle-switch::after {
-	@apply bg-transparent border-ST0;
-}
+	&:active {
+		&:before {
+			@apply scale-75;
+		}
+	}
 
-.toggle-switch-container .toggle-switch.toggle-switch-checked::after {
-	@apply bg-current;
-}
-
-.toggle-switch-container.toggle-switch-container-disabled .toggle-switch.toggle-switch-checked::after {
-	@apply bg-ST0;
+	&:disabled {
+		@apply opacity-50 pointer-events-none;
+	}
 }
 </style>
