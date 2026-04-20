50a9e0d2f4549141a669e6128f75a62200820949
Thomas Riedel <thomas.riedel@worldiety.de>
2026-04-20 14:48:46 +0200

    chore: frontend rebuild

--- web/vuejs/dist/modern/assets/UiUnknownType-DynwoUys.js
+++
@@ -1,1 +0,0 @@
-import{d as s,b as o,f as n,a0 as i}from"./main-CWjL2C55.js";const c={key:0},a={key:1},u=s({__name:"UiUnknownType",props:{ui:{}},setup(t){const e=t;return(p,r)=>e.ui?(n(),o("div",c,"[Protocol type has not been implemented: '"+i(e.ui.constructor.name)+"']",1)):(n(),o("div",a,"ui component is null"))}});export{u as default};
