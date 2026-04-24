fa5f8c82b41cc9eeb0ab16edca94c99e5a9c763e
Thomas Riedel <thomas.riedel@worldiety.de>
2026-04-24 12:02:11 +0200

    feat: cancel vue app initialization for crawler user agents

--- web/vuejs/src/main.ts
+++ web/vuejs/src/main.ts
@@ -16,27 +16,35 @@ import WebSocketAdapter from '@/shared/network/webSocketAdapter';
 import ThemeManager from '@/shared/themeManager';
 import '@/assets/css/style.css';
 import '@/assets/css/tailwind.css';
+import crawlerUserAgents from 'crawler-user-agents';
 
-const pinia = createPinia();
-
-const app = createApp(App);
-
-app.provide(serviceAdapterKey, new WebSocketAdapter());
-app.provide(uploadRepositoryKey, new UploadRepository());
-app.provide(themeManagerKey, new ThemeManager());
-
-app.directive('inline', (element: HTMLElement) => {
-	const parentCss = element.classList;
-	for (let i = 0; i < element.children.length; i++) {
-		for (let j = 0; j < parentCss.length; j++) {
-			const parentCssClass = parentCss.item(j);
-			const childElement = element.children.item(i);
-			if (childElement && parentCssClass) {
-				childElement.classList.add(parentCssClass);
-			}
-		}
-	}
-	element.replaceWith(...Object.values(element.children));
+const isCrawler = crawlerUserAgents.some((cua) => {
+	const regex = new RegExp(cua.pattern, 'i');
+	return regex.test(navigator.userAgent);
 });
 
-app.use(pinia).use(i18n).mount('#app');
+if (!isCrawler) {
+	const pinia = createPinia();
+
+	const app = createApp(App);
+
+	app.provide(serviceAdapterKey, new WebSocketAdapter());
+	app.provide(uploadRepositoryKey, new UploadRepository());
+	app.provide(themeManagerKey, new ThemeManager());
+
+	app.directive('inline', (element: HTMLElement) => {
+		const parentCss = element.classList;
+		for (let i = 0; i < element.children.length; i++) {
+			for (let j = 0; j < parentCss.length; j++) {
+				const parentCssClass = parentCss.item(j);
+				const childElement = element.children.item(i);
+				if (childElement && parentCssClass) {
+					childElement.classList.add(parentCssClass);
+				}
+			}
+		}
+		element.replaceWith(...Object.values(element.children));
+	});
+
+	app.use(pinia).use(i18n).mount('#app');
+}
