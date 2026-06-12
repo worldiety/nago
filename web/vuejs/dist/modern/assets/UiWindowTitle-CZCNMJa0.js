268d4a7ebf7bb606580890ccccd0627ade245734
Thomas Riedel <thomas.riedel@worldiety.de>
2026-06-12 08:35:34 +0200

    chore: frontend rebuild

---
+++ web/vuejs/dist/modern/assets/UiWindowTitle-CZCNMJa0.js
@@ -0,0 +1,1 @@
+import{d as o,b as a,t as n,bh as i}from"./main-DM20CFq5.js";const p=o({__name:"UiWindowTitle",props:{ui:{}},setup(t){const e=t;return e.ui.value&&(document.title=e.ui.value),(r,s)=>(a(),n(i,{to:"body"}))}});export{p as default};
